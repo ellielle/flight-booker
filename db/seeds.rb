46aab129627d43d099ac97a6ec9ead64623d3662
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-25 06:40:18 -0400

    add seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ airport2 = Airport.create!(name: "John F. Kennedy International Airport", code: 
 airport3 = Airport.create!(name: "San Francisco International Airport", code: "SFO")
 
 # Create Flights
-flight1 = Flight.create!(origin: airport1.id, destination: airport3.id,
+flight1 = Flight.create!(from_airport_id: airport1.id, to_airport_id: airport3.id,
                          departure: 3.hours.ago, duration: 293)
-flight2 = Flight.create!(origin: airport2.id, destination: airport3.id,
+flight2 = Flight.create!(from_airport_id: airport2.id, to_airport_id: airport3.id,
                          departure: 2.hours.ago, duration: 300)
-flight3 = Flight.create!(origin: airport3.id, destination: airport1.id,
+flight3 = Flight.create!(from_airport_id: airport3.id, to_airport_id: airport1.id,
                          departure: 2.hours.from_now, duration: 260)
