70d8e0e0e54ec420357958fa0d5a1eb4ad9c16a6
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-07 06:00:44 -0400

    remove unnecessary methods

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,12 +1,4 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, foreign_key: :from_airport_id, class_name: "Airport"
   belongs_to :to_airport, foreign_key: :to_airport_id, class_name: "Airport"
-
-  def flight_date_format
-    departure.strftime("%m/%d/%Y - %H:%m")
-  end
-
-  def flight_date_only_format
-    departure.strftime("%m/%d/%Y")
-  end
 end
