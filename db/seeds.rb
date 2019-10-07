dae6759de550c321dae3fef1f1f4463223fc9b8a
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-07 02:41:41 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Flight.delete_all
 
 def self.create_flight(from_airport, to_airport)
   Flight.create!(from_airport_id: from_airport.id, to_airport_id: to_airport.id,
-                 departure: Faker::Time.between_dates(from: DateTime.now, to: DateTime.now + 20),
+                 departure: Faker::Time.between_dates(from: DateTime.now + 2, to: DateTime.now + 20),
                  duration: rand(345..680))
 end
 
