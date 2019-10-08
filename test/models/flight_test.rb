ff0d1342098801af6c18994256e07448ec7ad00d
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-08 02:35:04 -0400

    initial test files

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @flight = Flight.new(from_airport_id: 3, to_airport_id: 5, departure: DateTime.now + 3,
+                         duration: 300)
+  end
+
+  test "should be a valid flight" do
+    @flight.valid?
+  end
 end
