95e3e31bbbe4449598ff20b2b98224be9bdffdf5
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-08 02:19:05 -0400

    add helper to remember user's form choices

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,11 @@
 module FlightsHelper
+  def keep_selected_forms(attr)
+    unless params[:flight].nil?
+      if params[:flight][attr.to_sym].nil? || params[:flight][attr.to_sym].empty?
+        1
+      else
+        params[:flight][attr.to_sym]
+      end
+    end
+  end
 end
