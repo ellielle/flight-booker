eaa39d5beb42b5d34d3c3cdc37f02409ae3d72bf
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-14 05:32:44 -0400

    add route for new booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "flights#index"
+  get "book_flight", to: "bookings#new"
   resources :flights, only: [:index]
   resources :bookings, only: [:new, :show, :create]
 end
