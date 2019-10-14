2dc8d1c037dcfa1c43246ac0f5f65c4c691c9bd1
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-14 05:16:47 -0400

    fix associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :booked_flight, class_name: "Flight"
-  belongs_to :booking, class_name: "Passenger"
+  belongs_to :passenger, class_name: "Flight"
+  belongs_to :scheduled_flight, class_name: "Passenger"
 end
