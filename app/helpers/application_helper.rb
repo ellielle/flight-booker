7a9219bef0f8060f98a7771bc577208940263f06
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-14 07:47:16 -0400

    add title helper and titles to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title_helper(page_title = '')
+    base_title = "Simple Flights"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
