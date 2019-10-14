bf32172827eaa508e18d7bc4e3e6afd58deeee25
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-14 05:04:38 -0400

    update associations for through table

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, foreign_key: :from_airport_id, class_name: "Airport"
   belongs_to :to_airport, foreign_key: :to_airport_id, class_name: "Airport"
+  has_many :bookings, foreign_key: :booked_flight_id
+  has_many :passengers, through: :bookings, source: :passenger_id
   scope :sorted_dates, -> { where("departure > ?", Time.now).order(:departure) }
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,4 @@
 class Passenger < ApplicationRecord
+  has_many :bookings, foreign_key: :booking_id
+  has_many :scheduled_flights, through: :bookings, source: :scheduled_flight
 end
