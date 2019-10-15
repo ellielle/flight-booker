8cc185ca297cbf3d846e37c9e4747a7dcf6a6dbd
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-15 06:36:19 -0400

    set up variables for partial and booking flight

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.where(id: params[:results][:chosen_flight])
+    @passengers = params[:results][:num_passengers]
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def booking_params
+
+  end
 end
