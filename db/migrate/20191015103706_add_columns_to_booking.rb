9008a7aa1b90e8b49bfd4cef471279116e212419
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-15 06:45:07 -0400

    screwed up, rolling back bookings

--- db/migrate/20191015103706_add_columns_to_booking.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToBooking < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bookings, :first_name, :string
-    add_column :bookings, :last_name, :string
-    add_column :bookings, :email, :string
-    add_column :bookings, :address, :string
-    add_index :bookings, :email
-  end
-end
