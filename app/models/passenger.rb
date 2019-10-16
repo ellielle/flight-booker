7cd9a4e1892cb51def6244bc62b58b88f3c3e597
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-16 06:27:15 -0400

    add validations

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,14 @@
 class Passenger < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  before_save :downcase_email
   has_many :bookings, foreign_key: :booking_id
   has_many :scheduled_flights, through: :bookings, source: :scheduled_flight
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
