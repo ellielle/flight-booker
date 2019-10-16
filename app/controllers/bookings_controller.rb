2fcc30b1349b3e84ff776b36b7ac915dfd6bd825
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-16 06:29:01 -0400

    grab record properly, basic submit function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,16 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
-    @flight = Flight.where(id: params[:results][:chosen_flight])
+    @flight = Flight.where(id: params[:results][:chosen_flight]).first
     @passengers = params[:results][:num_passengers]
   end
 
   def create
+    if params[:commit] == "Submit Information"
 
+    elsif params[:commit] == "Change Details"
+      redirect_to root_path
+    end
   end
 
   def show
