3b94377f05b565706b767ce6491934b83094e0e8
ellielle <40385743+ellielle@users.noreply.github.com>
2019-10-21 09:49:29 -0400

    edit routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "flights#index"
   get "book_flight", to: "bookings#new"
+  post "search", to: "bookings#search"
   resources :flights, only: [:index]
   resources :bookings, only: [:new, :show, :create]
 end
